63225564ea57b577dd3979267e74c9ed1579a412
ooo-mmm <31175327+ooo-mmm@users.noreply.github.com>
2025-04-10 15:58:25 +0530

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -11,9 +11,6 @@
   outputs = { self, nixpkgs, nixvim, flake-parts } @ inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
-        "aarch64-darwin"
-        "aarch64-linux"
-        "x86_64-darwin"
         "x86_64-linux"
       ];
 
